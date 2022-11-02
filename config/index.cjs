4633d1fe6428e815b9989fae5c4341d5b6825342
chencaixin <117144831+chencaixin@users.noreply.github.com>
2022-11-02 19:39:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7scX6PYQkaTB2r8_7mUulkguYiE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ARKODqkJ8bemwlkoiIUwSEJihI41BEEhVdLymualeGA',
+      useTemplateId: 'UZ7cppjN0XW2wJVNCBjOWSld-bNT2RlZtUkU3fbmqMc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-19',
       festivals: [
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝贝', year: '2001', date: '08-19',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '01-01',
