733c3ce32ff02f9a6ca5ff9dcb3d920262a799ae
chencaixin <117144831+chencaixin@users.noreply.github.com>
2022-11-03 16:03:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,12 +19,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '鑫哥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7scX6PYQkaTB2r8_7mUulkguYiE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '5g2S3CWVDiApWhBvds0oilNWaEGjMMu156UE-s5UQpc',
+      useTemplateId: 'rqwdH4iXI_-xCMnGqdmVeqC-hxNpOrvjnbhmZlxR0rk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscope: true,
       horoscopeDate: '08-19',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
