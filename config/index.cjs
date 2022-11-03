290f5ca2ef070e4c11f1841b0726259bb7e80fe1
chencaixin <117144831+chencaixin@users.noreply.github.com>
2022-11-03 23:19:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,9 +23,9 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7scX6PYQkaTB2r8_7mUulkguYiE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'uT91Xo2ujNZrxxazN1Bz9jTs-POIvaAmLTR7XT6HjQY',
+      useTemplateId: '8ARlYm2hUki_RPLRiqHECPlWDDQU7DkfnJa-rUbLdsI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '08-19',
+      horoscopeDate: '09-26',
       horoscopeDateType: '今日',
        openUrl: 'https://wangxinleo.cn',
       festivals: [
