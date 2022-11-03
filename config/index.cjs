926349ac76342a47c39711ae1500d26cb50affe4
chencaixin <117144831+chencaixin@users.noreply.github.com>
2022-11-03 22:39:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7scX6PYQkaTB2r8_7mUulkguYiE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	tYy6rg62vEgwfNQmdk3pK87GTyvPdH7uXhORy8HQhQY',
+      useTemplateId: 'tYy6rg62vEgwfNQmdk3pK87GTyvPdH7uXhORy8HQhQY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-19',
       horoscopeDateType: '今日',
@@ -57,7 +57,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'gSm5dA39hESjoVq4S0uFKLaStemLqvLuKTJ14uQpJLI',
+  CALLBACK_TEMPLATE_ID: '-hjmEkOg3u7dUTEXaNtoha4Ut_LIwLqCJyZnWoyymt0',
 
   CALLBACK_USERS: [
     {
