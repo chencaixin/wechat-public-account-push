0b39e363105dd89f7408961f60781fe6189dd689
chencaixin <117144831+chencaixin@users.noreply.github.com>
2022-11-03 23:39:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '大宝贝漂亮依依崽',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o7scX6PYQkaTB2r8_7mUulkguYiE',
+      id: 'o7scX6BzTnTPtJSHAT9B1hiGy2ls',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'FKRK7M6PcyvqEhMda6zDp7QCHEb7jA-ztfdZk8gmMN0',
+      useTemplateId: 'wQBgJPK-fJnOCpsi_ZhelYTKC_CTu75-_ZuXuiUUMzQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-26',
       horoscopeDateType: '今日',
