68bab9b81a65ae167b219db77c36f31efbcf15b6
chencaixin <117144831+chencaixin@users.noreply.github.com>
2023-04-09 23:21:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,6 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+USE_PAKKAGE:'push-plus',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx87eff2233bfd3b05',
@@ -19,9 +20,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '大宝贝漂亮依依崽',
+      name: '鑫哥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o7scX6BzTnTPtJSHAT9B1hiGy2ls',
+      id: 'o7scX6Czh7Ce-jx9klvwt80uflN0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'FKRK7M6PcyvqEhMda6zDp7QCHEb7jA-ztfdZk8gmMN0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
